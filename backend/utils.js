eb33cd31c2315f70077893c9f9a32c8e1bee158e
Ed Code <edvard.ekstrom@gmail.com>
2023-09-19 09:53:37 +0200

    timestamp fix

--- backend/utils.js
+++ backend/utils.js
@@ -1,13 +1,13 @@
-const timestamp = new Date();
 
 const leadingZero = (num) => `0${num}`.slice(-2);
 
 const formatTime = (date) =>
-  [date.getHours(), date.getMinutes(), date.getSeconds()]
-    .map(leadingZero)
-    .join(":");
+[date.getHours(), date.getMinutes(), date.getSeconds()]
+.map(leadingZero)
+.join(":");
 
 const postChatMessage = (io, data, message) => {
+  const timestamp = new Date();
   io.in(data.room).emit(
     "recieve_message",
     `[${formatTime(timestamp)}]: ${message}`
